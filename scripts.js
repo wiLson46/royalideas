80058550538ac26da22389455c08ce182accd5b7
wiLson <theroyalideas@gmail.com>
2020-04-07 16:38:57 -0300

    v0.50

--- scripts.js
+++ scripts.js
@@ -37,18 +37,29 @@ function enviarMail() {
             if (document.getElementById("inputPhone").value != "" && document.getElementById("inputTime").value != "") {
                 document.getElementById("myForm").submit();
                 document.getElementById("myForm").reset();
+                document.getElementById("myForm").classList.add("d-none");
+                document.getElementById("sentSuccess").classList.remove("d-none");
                 showContent();
             }
         } 
         else {
             document.getElementById("myForm").submit();
             document.getElementById("myForm").reset();
+            document.getElementById("myForm").classList.add("d-none");
+            document.getElementById("sentSuccess").classList.remove("d-none");
+            showContent();
         }
+        
+    }
+    else {
+        document.getElementById("myForm").classList.add("was-validated");
+    }
+}
 
-    }
-    
-    function clearForm(){
-        document.getElementById("myForm").reset();
-        showContent();
-    }
+function clearForm() {
+    document.getElementById("myForm").reset();
+    showContent();
+    document.getElementById("sentSuccess").classList.add("d-none");
+    document.getElementById("myForm").classList.remove("d-none");
+    document.getElementById("myForm").classList.remove("was-validated");
 }
